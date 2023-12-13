c760bcecee34118e7d2e7ab5f883581baa4df80f
andywhodat <2695845@collegelacite.ca>
2023-12-13 02:33:46 -0500

    vue

---
+++ Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Count: {{ counter.count }}</p>
+    <button @click="counter.increment">Increment</button>
+    <button @click="counter.decrement">Decrement</button>
+    <button @click="counter.reset">Reset</button>
+    <p v-if="counter.isPositive">Count is positive!</p>
+  </div>
+</template>
+
+<script>
+import { useCounterStore } from '@/store/counterStore';
+
+export default {
+  setup() {
+    const counter = useCounterStore();
+
+    return {
+      counter,
+    };
+  },
+};
+</script>
